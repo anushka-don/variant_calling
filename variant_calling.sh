fc532e5a9654a54d050c7c88611414e0ccb1ebbb
Anushka Dongaonkar <d13anushka@gmail.com>
2025-10-31 17:28:26 -0400

    variant calling performed on the reads

--- variant_calling.sh
+++ variant_calling.sh
@@ -1,46 +1,53 @@
 #!bin/bash
 
+# Create Directories
+mkdir data
+mkdir ref_genome
+mkdir results
+
 # Download the data if not already done so
 echo "  Downloading reads data and ref genome  "
 
-curl -o read1.fastq.gz https://ftp.sra.ebi.ac.uk/vol1/fastq/SRR258/007/SRR2584867/SRR2584867_1.fastq.gz
+curl -o data/read1.fastq.gz https://ftp.sra.ebi.ac.uk/vol1/fastq/SRR258/007/SRR2584867/SRR2584867_1.fastq.gz
 
-curl -o read2.fastq.gz https://ftp.sra.ebi.ac.uk/vol1/fastq/SRR258/007/SRR2584867/SRR2584867_2.fastq.gz
+curl -o data/read2.fastq.gz https://ftp.sra.ebi.ac.uk/vol1/fastq/SRR258/007/SRR2584867/SRR2584867_2.fastq.gz
 
-curl -o ref_genome.fna.gz https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/017/985/GCF_000017985.1_ASM1798v1/GCF_000017985.1_ASM1798v1_genomic.fna.gz
+curl -o ref_genome/ref_genome.fna.gz https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/017/985/GCF_000017985.1_ASM1798v1/GCF_000017985.1_ASM1798v1_genomic.fna.gz
 
 echo "Data Download Successful"
 echo "Unzipping data"
 
-gunzip *.gz
+gunzip data/*.gz
+gunzip ref_genome/*.gz
 
 
 # Indexing Reference Genome using bwa
 echo "Creating index for reference genome"
-
+cd ref_genome
 bwa index ref_genome.fna
+cd ..
 
 
 # Aligning reads to ref
 echo "Aligning reads to index"
 
-bwa mem ref_genome.fna read1.fastq read2.fastq >output.sam
+bwa mem ref_genome/ref_genome.fna data/read1.fastq data/read2.fastq >results/aligned.sam
 
 
 # Convert, Sort, and Index BAM
 echo "Converting SAM to BAM, sorting, and indexing..."
 
-samtools view -Sb output.sam > output.bam
+samtools view -Sb results/aligned.sam > results/aligned.bam
 
-samtools sort output.bam -o output_sorted.bam
+samtools sort results/aligned.bam -o results/sorted_aligned.bam
 
-samtools index output_sorted.bam
+samtools index results/sorted_aligned.bam
 
 
 # Variant Calling
 echo "Variant Calling"
 
-bcftools mpileup -Ou -f ref_genome.fna output_sorted.bam | bcftools call -mv -Ob -o calls.bcf
+bcftools mpileup -Ou -f ref_genome/ref_genome.fna results/sorted_aligned.bam | bcftools call -mv -Ob -o results/calls.bcf
 
-bcftools view calls.bcf >calls.vcf
+bcftools view results/calls.bcf > results/calls.vcf
 
